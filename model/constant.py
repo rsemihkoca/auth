29909c7f68a583c01b24128754cf9ff31a4f0fad
Riza Semih Koca <rsemihkoca@outlook.com>
2024-07-16 12:53:42 +0300

    first working solution:model/constant.py

--- model/constant.py
+++ model/constant.py
@@ -1,3 +1,13 @@
 
 
 class Constant:
+
+    class Datetime:
+        class Format:
+            DATE = "%Y-%m-%d"
+            TIME = "%H:%M:%S"
+            DATETIME = "%Y-%m-%d %H:%M:%S"
+
+    class Broker:
+        HOST = "emqx.local"
+        PORT = 1883
