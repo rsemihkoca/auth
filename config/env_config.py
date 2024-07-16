dec95ca0ea2c999d28121a39b8ef219e4adc1608
Riza Semih Koca <rsemihkoca@outlook.com>
2024-07-16 12:53:42 +0300

    first working solution:config/env_config.py

---
+++ config/env_config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+class EnvironmentConfig:
+    def __init__(self, env_file='.env'):
+        self.env_file = env_file
+        self.load_env()
+
+    def load_env(self):
+        load_dotenv(self.env_file)
+
+    def get(self, key, default=None):
+        return os.getenv(key, default)
+
+
