b6a47e26535d5e4a2a7c37054fe3e629e5d51011
Riza Semih Koca <rsemihkoca@outlook.com>
2024-07-16 12:53:42 +0300

    first working solution:database/models.py

--- database/models.py
+++ database/models.py
@@ -4,7 +4,11 @@ from database.db import Base
 class MQTTClient(Base):
     __tablename__ = "mqtt_client"
 
-    serial_number = Column(String, primary_key=True, index=True)
+    client_id = Column(String, primary_key=True, index=True)
+    username = Column(String, primary_key=True, index=True)
+    password = Column(String)
+    hashed_password = Column(String)
+    is_superuser = Column(Boolean, default=False)
     target = Column(String, default="")
     is_disabled = Column(Boolean, default=False)
     last_update_time = Column(String)
